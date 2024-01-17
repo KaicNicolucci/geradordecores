6ed29d74c1f61641b37cd22bf1dcc3dc4cb5c373
kaic nicoluci <kaic.nicolucci@gmail.com>
2024-01-17 20:28:51 -0300

    solucao do codigo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const button = document.querySelector('button');
 let colors = ["red","green", "blue", "gray", "orange", "pink", "purple"];
+let spanText = document.querySelector('.code-color');
 
 function gerarCor(items) {
     let corAleatoria = Math.floor(Math.random() * colors.length);
-    items[corAleatoria];
+    return items[corAleatoria];
 };
 
-console.log(gerarCor(colors));
+
+
+button.addEventListener("click", function(){
+    `${gerarCor(colors)}`
+    document.body.style.backgroundColor = `${gerarCor(colors)}`  
+    spanText.innerHTML = `${gerarCor(colors)}`
+})
