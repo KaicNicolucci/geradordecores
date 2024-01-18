2db42a32ec1970d8e096af16aa7f99cbe2a7dd0e
kaic nicoluci <kaic.nicolucci@gmail.com>
2024-01-18 02:02:59 -0300

    adicionando funcao de array

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+let coresSorteadas = [];
 const button = document.querySelector('button');
 let colors = ["red","green", "blue", "gray", "orange", "pink", "purple", "yellow", "black"];
 let spanText = document.querySelector('.code-color');
 
 function gerarCor(items) {
     let corAleatoria = Math.floor(Math.random() * colors.length);
-    return items[corAleatoria];
+    
+    let indiceLista = coresSorteadas.length
+    if (indiceLista == colors.length) {
+        coresSorteadas = [];
+        
+    }
+    
+    if (coresSorteadas.includes(corAleatoria)) {
+        return  gerarCor(items);
+    } else {
+        coresSorteadas.push(corAleatoria);
+        console.log(coresSorteadas)
+        return items[corAleatoria];
+    }
 };
 
 
 
 button.addEventListener("click", function(){
-    let corGerada = gerarCor(colors)
-    document.body.style.backgroundColor = corGerada  
-    spanText.innerHTML = ` ${corGerada}`
-})
+    let corGerada = gerarCor(colors);
+    document.body.style.backgroundColor = corGerada;
+    spanText.innerHTML = ` ${corGerada}`;
+});
+
+
+console.log(colors.length)
